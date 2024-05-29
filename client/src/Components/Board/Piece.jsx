16c466432b1b6fce3857d8e73155df867bb2bfeb
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-05-29 22:41:16 +0400

    optimization

--- client/src/Components/Board/Piece.jsx
+++ client/src/Components/Board/Piece.jsx
@@ -1,44 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./board.module.css";
-import wp from '../../assets/Pieces_Imgs/wp.png'
-import bp from '../../assets/Pieces_Imgs/bp.png'
+import wp from "../../assets/Pieces_Imgs/wp.png";
+import bp from "../../assets/Pieces_Imgs/bp.png";
 const Piece = ({ pc, rank, file }) => {
+  const [isGrabbing, setIsGrabbing] = useState(false);
+
   const onDragStart = (e) => {
-    e.dataTransfer.setData("text/plain", `${pc},${rank},${file}`);
+    e.dataTransfer.setData('text/plain', `${pc},${rank},${file}`);
     setTimeout(() => {
-      e.target.style.display = "none";
+      e.target.style.display = 'none';
     }, 0);
+    setIsGrabbing(true);
   };
+
+  const onDragEnd = (e) => {
+    e.target.style.display = 'block';
+    setIsGrabbing(false);
+  };
+
+  const imgSrc = pc === 'bp' ? bp : wp;
+  const imgClass = pc === 'bp' ? style.black_b : style.white_b;
+
   return (
-    <>
-      {pc === "bp" ? (
-        <img
-        src={bp}
-          style={{ cursor: "default" }}
-          className={style.black_b}
-          draggable="true"
-          onDragStart={(e) => onDragStart(e)}
-          onDragEnd={(e) => (e.target.style.display = "block")}
-          onDragOver={(e) => {
-            e.preventDefault();
-            e.dataTransfer.dropEffect = "move";
-          }}
-        ></img>
-      ) : (
-        <img
-        src={wp}
-          style={{ cursor: "default" }}
-          className={style.white_b}
-          draggable="true"
-          onDragStart={(e) => onDragStart(e)}
-          onDragEnd={(e) => (e.target.style.display = "block")}
-          onDragOver={(e) => {
-            e.preventDefault();
-            e.dataTransfer.dropEffect = "move";
-          }}
-        ></img>
-      )}
-    </>
+    <img
+      src={imgSrc}
+      style={{ cursor: isGrabbing ? 'grabbing' : 'grab' }}
+      className={imgClass}
+      draggable="true"
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      onDragOver={(e) => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
+      }}
+    />
   );
 };
+
 export default Piece;
